cfd97f8e82db816f627c1c6757a61efb1f7d1438
EmG9999 <emilianomondorge@gmail.Com>
2025-02-06 14:19:41 +0100

    correction arboresecence du projet

---
+++ FamilyRoots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamilyRoots
+//
+//  Created by Apprenant 165 on 06/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamilyRoots/FamilyRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyRootsApp.swift
+//  FamilyRoots
+//
+//  Created by Apprenant 165 on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FamilyRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
